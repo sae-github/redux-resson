99e88a494427a9f352ccc45358d9a33c7fe18ed8
sae-github <miyasae1126@gmail.com>
2022-06-26 22:53:08 +0900

    refactor: style Adjustment

---
+++ createAsyncThunk/src/App.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from "./postsSlice";
+
+export const App = () => {
+  const { posts, loading, error, errorMessage } = useSelector(
+    (state) => state.posts
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div className="post">
+      {error && <p>{errorMessage}</p>}
+      {posts &&
+        posts.map((post) => (
+          <li key={post.id} className="post-list">
+            <p className="post-title">{post.title}</p>
+            <div className="post-body">{post.body}</div>
+          </li>
+        ))}
+    </div>
+  );
+};
